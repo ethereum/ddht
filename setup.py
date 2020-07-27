5d09cd4c91f81baaaac7c981c626856a80b5fd7b
Piper Merriam <pipermerriam@gmail.com>
2020-07-27 16:07:28 -0600

    missing dep

--- setup.py
+++ setup.py
@@ -61,6 +61,7 @@ setup(
     install_requires=[
         "cached-property>=1.5.1,<2",
         "coincurve>=10.0.0,<11.0.0",
+        "cryptography==3.0",
         "async-service==0.1.0a8",
         "eth-hash[pycryptodome]>=0.1.4,<1",
         "eth-keys>=0.3.3,<0.4.0",
