5f7d9cfcf75120233003c4f9875294c8f9846f9a
Piper Merriam <pipermerriam@gmail.com>
2020-07-29 10:28:09 -0600

    fix test

--- tests/core/test_cli_parsing_to_boot_info.py
+++ tests/core/test_cli_parsing_to_boot_info.py
@@ -14,29 +14,30 @@ KEY = keys.PrivateKey(KEY_RAW)
 
 
 @pytest.mark.parametrize(
-    "args,expected",
+    "args,factory_kwargs",
     (
-        ((), BootInfoFactory()),
-        (("--port", "12345"), BootInfoFactory(port=12345)),
+        ((), {}),
+        (("--port", "12345"), dict(port=12345)),
         (
             ("--listen-address", "192.168.0.1"),
-            BootInfoFactory(listen_on=ipaddress.ip_address("192.168.0.1")),
+            dict(listen_on=ipaddress.ip_address("192.168.0.1")),
         ),
         (
             ("--base-dir", "~/test-home-gets-resolved"),
-            BootInfoFactory(
+            dict(
                 base_dir=pathlib.Path("~/test-home-gets-resolved").expanduser()
             ),
         ),
         (
             ("--base-dir", "./../test-relative-gets-resolved"),
-            BootInfoFactory(
+            dict(
                 base_dir=pathlib.Path("./../test-relative-gets-resolved").resolve()
             ),
         ),
-        (("--private-key", KEY_HEX), BootInfoFactory(private_key=KEY)),
+        (("--private-key", KEY_HEX), dict(private_key=KEY)),
     ),
 )
-def test_cli_args_to_boot_info(args, expected):
+def test_cli_args_to_boot_info(args, factory_kwargs):
+    expected = BootInfoFactory(**factory_kwargs)
     actual = BootInfo.from_cli_args(args)
     assert actual == expected
