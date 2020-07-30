ce5aff49deccfd72ec02e7834608314a06d22484
Piper Merriam <pipermerriam@gmail.com>
2020-07-30 09:38:34 -0600

    Parse bootnode ENRs in cli parser

--- ddht/cli_parser.py
+++ ddht/cli_parser.py
@@ -1,9 +1,11 @@
 import argparse
 import ipaddress
 import pathlib
+from typing import Any
 
 from ddht import __version__
 from ddht.cli_commands import do_main
+from ddht.enr import ENR
 
 parser = argparse.ArgumentParser(description="Discovery V5 DHT")
 parser.set_defaults(func=do_main)
@@ -40,13 +42,41 @@ logging_parser.add_argument(
     help=("Configure the logging level. "),
 )
 
+
 #
 # Network
 #
+class NormalizeAndAppendENR(argparse.Action):
+    def __call__(
+        self,
+        parser: argparse.ArgumentParser,
+        namespace: argparse.Namespace,
+        value: Any,
+        option_string: str = None,
+    ) -> None:
+        if value is None:
+            return
+
+        enr = ENR.from_repr(value)
+
+        if getattr(namespace, self.dest) is None:
+            setattr(namespace, self.dest, ())
+
+        enr_list = getattr(namespace, self.dest)
+        enr_list += (enr,)
+
+        setattr(namespace, self.dest, enr_list)
+
+
 network_parser.add_argument(
     "--port", type=int, help="Port number for the discovery service"
 )
 network_parser.add_argument(
     "--listen-address", type=ipaddress.ip_address, help="IP address to listen on"
 )
-network_parser.add_argument("--bootnodes", help="IP address to listen on")
+network_parser.add_argument(
+    "--bootnode",
+    action=NormalizeAndAppendENR,
+    help="IP address to listen on",
+    dest="bootnodes",
+)
