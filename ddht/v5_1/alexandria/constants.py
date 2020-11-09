3c4f918d3c26411c2cfb8077433c958f0f629020
Piper Merriam <pipermerriam@gmail.com>
2020-11-09 09:29:21 -0700

    More precise upper bounds on Alexandria payload sizes

--- ddht/v5_1/alexandria/constants.py
+++ ddht/v5_1/alexandria/constants.py
@@ -1,5 +1,7 @@
 from typing import Tuple
 
+from ddht.constants import DISCOVERY_MAX_PACKET_SIZE
+
 ALEXANDRIA_PROTOCOL_ID = b"alexandria"
 
 DEFAULT_BOOTNODES: Tuple[str, ...] = ()
@@ -10,3 +12,14 @@ GB = 1024 * 1024 * 1024  # 2**30
 
 # All of the powers of two
 POWERS_OF_TWO = tuple(2 ** n for n in range(256))
+
+# Safe upper bound for the raw payload of an alexandria packet.  We expect the following overhead:
+#
+# - 23 bytes for the packet header
+# - up-to 34 bytes of packet overhead.
+# - up-to 4 bytes for request_id
+# - 10 bytes for the `protocol_id`
+# - 1 byte for the alexandria message type
+# - up-to 10 bytes for RLP encoding overhead.
+#
+MAX_PAYLOAD_SIZE = DISCOVERY_MAX_PACKET_SIZE - 90
