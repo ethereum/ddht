3f50151a3bf25af4de4775d345e5e8158a5e4c6c
Piper Merriam <pipermerriam@gmail.com>
2020-11-18 07:46:56 -0700

    Implement Advertisement data model

---
+++ ddht/tools/factories/alexandria.py
@@ -0,0 +1,41 @@
+import datetime
+import secrets
+
+import factory
+
+from ddht.tools.factories.keys import PrivateKeyFactory
+from ddht.v5_1.alexandria.advertisements import (
+    Advertisement,
+    create_advertisement_signature,
+)
+from ddht.v5_1.alexandria.content import content_key_to_content_id
+
+ONE_HOUR = datetime.timedelta(seconds=60 * 60)
+
+
+class AdvertisementFactory(factory.Factory):  # type: ignore
+    content_key = factory.LazyFunction(
+        lambda: secrets.token_bytes(33 + secrets.randbelow(127))
+    )
+    hash_tree_root = factory.LazyFunction(lambda: secrets.token_bytes(32))
+    expires_at = factory.LazyFunction(
+        lambda: datetime.datetime.utcnow().replace(microsecond=0) + ONE_HOUR
+    )
+
+    signature_v = factory.LazyAttribute(lambda o: o.signature.v)
+    signature_r = factory.LazyAttribute(lambda o: o.signature.r)
+    signature_s = factory.LazyAttribute(lambda o: o.signature.s)
+
+    class Params:
+        private_key = factory.SubFactory(PrivateKeyFactory)
+        signature = factory.LazyAttribute(
+            lambda o: create_advertisement_signature(
+                content_id=content_key_to_content_id(o.content_key),
+                hash_tree_root=o.hash_tree_root,
+                expires_at=o.expires_at,
+                private_key=o.private_key,
+            )
+        )
+
+    class Meta:
+        model = Advertisement
--- ddht/v5_1/alexandria/sedes.py
+++ ddht/v5_1/alexandria/sedes.py
@@ -1,4 +1,14 @@
-from ssz.sedes import Container, List, boolean, uint8, uint16, uint32
+from ssz.sedes import (
+    Container,
+    List,
+    UInt,
+    boolean,
+    bytes32,
+    uint8,
+    uint16,
+    uint32,
+    uint256,
+)
 
 from ddht.v5_1.alexandria.constants import GB
 
@@ -15,14 +25,22 @@ class ByteList(List):  # type: ignore
 
 
 byte_list = ByteList(max_length=2048)
+uint40 = UInt(40)
 
 
 PingSedes = Container(field_sedes=(uint32,))
 PongSedes = Container(field_sedes=(uint32,))
+
 FindNodesSedes = Container(field_sedes=(List(uint16, max_length=256),))
 FoundNodesSedes = Container(field_sedes=(uint8, List(byte_list, max_length=32)))
+
 GetContentSedes = Container(field_sedes=(byte_list, uint32, uint16))
 ContentSedes = Container(field_sedes=(boolean, byte_list,))
+
+AdvertisementSedes = Container(
+    field_sedes=(byte_list, bytes32, uint40, uint8, uint256, uint256)
+)
+AdvertiseSedes = List(AdvertisementSedes, max_length=32)
 
 # sedes used for encoding alexandria content
 content_sedes = ByteList(max_length=GB)
